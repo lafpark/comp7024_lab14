3fc59ca216231e5a89d2ed04e718281c39295b2a
Laurence Park <lapark@westernsydney.edu.au>
2022-10-24 11:39:43 +1100

    Added script to plot hospital beds needed.

---
+++ hospital_beds.R
@@ -0,0 +1,13 @@
+
+
+d <- read.csv("epiSEIHCRD_combAge.csv")
+d$t <- as.Date(d$t, origin = "2020-03-01")
+
+
+chosen <- which((d$t >= "2020-07-01") & (d$t < "2020-12-31"))
+
+plot(d$t[chosen], d$H[chosen], xlab = "Date", ylab = "Number of Beds",
+	main = "Hospital Beds Needed")
+
+
+
