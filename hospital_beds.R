d54d76e326c1cedcee44aff109b2d0e1a65870e3
Laurence Park <lapark@scem.uws.edu.au>
2022-10-24 11:52:41 +1100

    Made plot line red.

--- hospital_beds.R
+++ hospital_beds.R
@@ -1,4 +1,5 @@
 
+# setwd("/tmp/comp7024_lab14/")
 
 d <- read.csv("epiSEIHCRD_combAge.csv")
 d$t <- as.Date(d$t, origin = "2020-03-01")
@@ -7,7 +8,7 @@ d$t <- as.Date(d$t, origin = "2020-03-01")
 chosen <- which((d$t >= "2020-07-01") & (d$t < "2020-12-31"))
 
 plot(d$t[chosen], d$H[chosen], xlab = "Date", ylab = "Number of Beds",
-	main = "Hospital Beds Needed")
+	main = "Hospital Beds Needed", col = "red", type = "l")
 
 
 
