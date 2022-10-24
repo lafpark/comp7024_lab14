d3c0ab1e280ca9dd69fef0325870d6493be9d335
Laurence Park <lapark@scem.uws.edu.au>
2022-10-24 12:09:40 +1100

    Changed to log scale on y axis.

--- hospital_beds.R
+++ hospital_beds.R
@@ -8,7 +8,7 @@ d$t <- as.Date(d$t, origin = "2020-03-01")
 chosen <- which((d$t >= "2020-07-01") & (d$t < "2020-12-31"))
 
 plot(d$t[chosen], d$H[chosen], xlab = "Date", ylab = "Number of Beds",
-	main = "Hospital Beds Needed", col = "red", type = "l")
+	main = "Hospital Beds Needed", col = "red", type = "l", log = "y")
 
 
 
